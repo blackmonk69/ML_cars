2cabb52d01d55e771e6bac57f098752dd8aa6137
Ariel Chocobar <arielteacher4@gmail.com>
2025-06-18 16:02:16 -0300

    dsf

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ This project has got many ingredientes: Obviously first of all the use of machin
 Once the model has been trained and we compare the models to find the most accurate, the best model is dumped into a file using pickle. This file is used to make a prediction in the predictor.py file. To set up the web page we decided to use Streamlit library, whichi is very powerful for simple apps and very easy to learn. This framework, like NiceGui or Taipy are based upon the idea that we can build a webpage using python entirely without resorting to more complex solutions like Flask or Django. You do not need to now javascript or html to build a website using Streamlit. Plus Streamlit can host your webpage for free. 
 
 you can visit the page we created at https://ml-cars.streamlit.app/
+
+![alt text](image.png "Title")
--- predictor.py
+++ predictor.py
@@ -2,18 +2,19 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import pickle
-import joblib
-
+# let us load the trained model
 model=pickle.load(open('LinearRegressionModel.pkl','rb'))
 
 car=pd.read_csv('Cleaned_Car_data.csv')
 st.title('Predicting Car Prices using ML - Linear Regression Model')
+#these lists help us populate the select boxes
 names=sorted(car['name'].unique())
 companies=sorted(car['company'].unique())
 car_models=sorted(car['name'].unique())
 years=sorted(car['year'].unique(),reverse=True)
 fuel_types=car['fuel_type'].unique()
 # 'name', 'company', 'year', 'kms_driven', 'fuel_type'
+
 names=st.selectbox("choose name",names)
 company = st.selectbox("Choose company",companies)
 car_model  = st.selectbox("Choose Car Model", car_models)
@@ -26,11 +27,7 @@ def predict(car_model,company,year,driven,fuel_type):
                               data=np.array([car_model,company,year,driven,fuel_type]).reshape(1, 5)))
     return round(prediction[0])
     
-    
-
 if st.button("Predict value"):
-    # Get the all values from predict function.
-    # score, pred_price, rsquare_score, mae, msle, rmse = predict(df, feature_list)
     prediccion='{:,}'.format(predict (car_model,company,year,driven,fuel_type))
     st.subheader (f"la prediccion es: $ {prediccion}")
     
