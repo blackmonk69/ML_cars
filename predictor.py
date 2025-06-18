3f0362e83abb8539a05bccccce864a5bd0e30ab2
Ariel Chocobar <arielteacher4@gmail.com>
2025-06-18 11:54:59 -0300

    dfa

--- predictor.py
+++ predictor.py
@@ -6,8 +6,10 @@ import numpy as np
 import pandas as pd
 
 import pickle
+import joblib
 
 model=pickle.load(open('LinearRegressionModel.pkl','rb'))
+model2=joblib.load('LinearRegressionModel2.joblib')
 car=pd.read_csv('Cleaned_Car_data.csv')
 st.title('Predicting Car Prices using ML and a Linear Regression Model')
 names=sorted(car['name'].unique())
@@ -26,7 +28,7 @@ year = st.selectbox("Choose year", years)
 fuel_type= st.selectbox("Choose fuel type",fuel_types)
 driven=st.text_input ('km driven')
 driven=3455
-prediction=model.predict(pd.DataFrame(columns=['name', 'company', 'year', 'kms_driven', 'fuel_type'],
+prediction=model2.predict(pd.DataFrame(columns=['name', 'company', 'year', 'kms_driven', 'fuel_type'],
                               data=np.array([car_model,company,year,driven,fuel_type]).reshape(1, 5)))
 print(prediction)
 st.write(f"la prediccion es {prediction}")
